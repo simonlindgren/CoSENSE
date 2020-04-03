2377c285cae5c4653a95f45c04e5945f52dc6b91
Samy Khatib <Samy.Khatib@nordnet.se>
2020-04-03 23:51:44 +0200

    feat: add sentiment map

--- src/DemoPages/Components/Maps/Examples/VectorMaps/index.js
+++ src/DemoPages/Components/Maps/Examples/VectorMaps/index.js
@@ -1,73 +1,36 @@
-import React, {Fragment} from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import {
-    Row, Col,
-    Card, CardBody,
-    CardTitle
-} from 'reactstrap';
+import React, { Fragment } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { Row, Col } from "reactstrap";
 
 // Examples
 
-import VectorMapsBasic from './Basic'
-import VectorMapsMarkers from './Markers'
-import VectorMapsDatasets from './Datasets'
-import VectorMapsAnimated from './Animated'
+import VectorMapsBasic from "./Basic";
+import VectorMapsMarkers from "./Markers";
+import VectorMapsDatasets from "./Datasets";
+import VectorMapsAnimated from "./Animated";
+import VectorMapsSentiment from "./SentimentMap";
 
 class VectorMapsExample extends React.Component {
-
-    render() {
-
-        return (
-            <Fragment>
-                <ReactCSSTransitionGroup
-                    component="div"
-                    transitionName="TabsAnimation"
-                    transitionAppear={true}
-                    transitionAppearTimeout={0}
-                    transitionEnter={false}
-                    transitionLeave={false}>
-                    <Row>
-                        <Col lg="6">
-                            <Card className="main-card mb-3">
-                                <CardBody>
-                                    <CardTitle>
-                                        Basic
-                                    </CardTitle>
-                                    <VectorMapsBasic/>
-                                </CardBody>
-                            </Card>
-                            <Card className="main-card mb-3">
-                                <CardBody>
-                                    <CardTitle>
-                                        Datasets
-                                    </CardTitle>
-                                    <VectorMapsDatasets/>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col lg="6">
-                            <Card className="main-card mb-3">
-                                <CardBody>
-                                    <CardTitle>
-                                        Animated
-                                    </CardTitle>
-                                    <VectorMapsAnimated/>
-                                </CardBody>
-                            </Card>
-                            <Card className="main-card mb-3">
-                                <CardBody>
-                                    <CardTitle>
-                                        Markers
-                                    </CardTitle>
-                                    <VectorMapsMarkers/>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </ReactCSSTransitionGroup>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <Row>
+            <Col lg="12">
+              <VectorMapsSentiment />
+            </Col>
+          </Row>
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
+  }
 }
 
 export default VectorMapsExample;
