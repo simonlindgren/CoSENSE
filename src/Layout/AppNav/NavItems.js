43d8f46eecd90a47d3f4dbf4592fd2b6fbdc7d0c
Samy Khatib <Samy.Khatib@nordnet.se>
2020-04-05 14:59:08 +0200

    fix: remove unnecessary nav items

--- src/Layout/AppNav/NavItems.js
+++ src/Layout/AppNav/NavItems.js
@@ -1,118 +1,29 @@
 export const MainNav = [
-    {
-        icon: 'pe-7s-rocket',
-        label: 'Dashboard Example',
-        to: '#/dashboards/basic',
-    },
+  {
+    icon: "pe-7s-rocket",
+    label: "Dashboard Example",
+    to: "#/dashboards/basic",
+  },
 ];
 export const SwedenNav = [
-    {
-        icon: 'lnr-smartphone',
-        label: 'Social media',
-        content: [
-            {
-                label: 'Twitter sentiment',
-                to: '#/socialmedia/twitter_sentiment',
-            },
-            // {
-            //     label: 'Dropdowns',
-            //     to: '#/elements/dropdowns',
-            //
-            // },
-            // {
-            //     label: 'Icons',
-            //     to: '#/elements/icons',
-            // },
-            // {
-            //     label: 'Badges',
-            //     to: '#/elements/badges-labels',
-            // },
-            // {
-            //     label: 'Cards',
-            //     to: '#/elements/cards',
-            // },
-            // {
-            //     label: 'List Groups',
-            //     to: '#/elements/list-group',
-            // },
-            // {
-            //     label: 'Navigation Menus',
-            //     to: '#/elements/navigation',
-            // },
-            // {
-            //     label: 'Utilities',
-            //     to: '#/elements/utilities',
-            // },
-        ],
-    },
-    {
-        icon: 'pe-7s-car',
-        label: 'GDELT',
-        content: [
-            {
-                label: 'Tabs',
-                to: '#/components/tabs',
-            },
-            {
-                label: 'Notifications',
-                to: '#/components/notifications',
-            },
-            {
-                label: 'Modals',
-                to: '#/components/modals',
-            },
-            {
-                label: 'Progress Bar',
-                to: '#/components/progress-bar',
-            },
-            {
-                label: 'Tooltips & Popovers',
-                to: '#/components/tooltips-popovers',
-            },
-            {
-                label: 'Carousel',
-                to: '#/components/carousel',
-            },
-            {
-                label: 'Maps',
-                to: '#/components/maps',
-            },
-        ],
-    },
-    {
-        icon: 'pe-7s-display2',
-        label: 'Regular Tables',
-        to: '#/tables/regular-tables',
-    },
-];
-export const FormsNav = [
-    {
-        icon: 'pe-7s-light',
-        label: 'Controls',
-        to: '#/forms/controls',
-    },
-    {
-        icon: 'pe-7s-eyedropper',
-        label: 'Layouts',
-        to: '#/forms/layouts',
-    },
-    {
-        icon: 'pe-7s-pendrive',
-        label: 'Validation',
-        to: '#/forms/validation',
-    },
-];
-export const WidgetsNav = [
-    {
-        icon: 'pe-7s-graph2',
-        label: 'Dashboard Boxes',
-        to: '#/widgets/dashboard-boxes',
-    },
-];
-export const ChartsNav = [
-    {
-        icon: 'pe-7s-graph2',
-        label: 'ChartJS',
-        to: '#/charts/chartjs',
-    },
+  {
+    icon: "lnr-smartphone",
+    label: "Social media",
+    content: [
+      {
+        label: "Twitter sentiment",
+        to: "#/socialmedia/twitter_sentiment",
+      },
+    ],
+  },
+  {
+    icon: "pe-7s-news-paper",
+    label: "Mass Media",
+    content: [
+      {
+        label: "Maps",
+        to: "#/components/maps",
+      },
+    ],
+  },
 ];
--- src/Layout/AppNav/VerticalNavWrapper.js
+++ src/Layout/AppNav/VerticalNavWrapper.js
@@ -1,32 +1,30 @@
-import React, {Component, Fragment} from 'react';
-import {withRouter} from 'react-router-dom';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-import MetisMenu from 'react-metismenu';
+import MetisMenu from "react-metismenu";
 
-import {MainNav, SwedenNav, FormsNav, WidgetsNav, ChartsNav} from './NavItems';
+import { SwedenNav } from "./NavItems";
 
 class Nav extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h5 className="app-sidebar__heading">Sweden</h5>
+        <MetisMenu
+          content={SwedenNav}
+          activeLinkFromLocation
+          className="vertical-nav-menu"
+          iconNamePrefix=""
+          classNameStateIcon="pe-7s-angle-down"
+        />
+      </React.Fragment>
+    );
+  }
 
-    state = {};
-
-    render() {
-        return (
-            <Fragment>
-                <h5 className="app-sidebar__heading">Sweden</h5>
-                <MetisMenu content={SwedenNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
-                <h5 className="app-sidebar__heading">Widgets</h5>
-                <MetisMenu content={WidgetsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
-                <h5 className="app-sidebar__heading">Forms</h5>
-                <MetisMenu content={FormsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
-                <h5 className="app-sidebar__heading">Charts</h5>
-                <MetisMenu content={ChartsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
-            </Fragment>
-        );
-    }
-
-    isPathActive(path) {
-        return this.props.location.pathname.startsWith(path);
-    }
+  isPathActive(path) {
+    return this.props.location.pathname.startsWith(path);
+  }
 }
 
 export default withRouter(Nav);
